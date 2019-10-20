d0f9a312d814b9c876548c0b12d83aec5a958c30
Ujjwal Singh <ujjwalsinghmuj@gmail.com>
2019-10-21 04:05:46 +0530

    font and css changes

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -4,7 +4,7 @@ const Scroll = (props) =>{
   
     
     return (
-        <div style={{overflowY:'scroll',border:'1px solid black', height:'800px' }}>
+        <div >
             {props.children}
         </div>
     )
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const SearchBox = ({searchfield,searchChange}) => {
+const SearchBox = ({ searchfield, searchChange }) => {
     return (
         <div className="pa2">
             <input
-                className="pa3 ba b--green bg-lightest-blue"
+                className="pa3 ba b--green br3 bg-lightest-blue"
                 type='search'
-                placeholder='search robots'
+                placeholder='Search Robots...'
                 onChange={searchChange}
             />
         </div>
